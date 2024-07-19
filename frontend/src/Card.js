edee8d091e244ee5a85bfee081f741339fc16d6a
heejinssss <heejin.dev33@gmail.com>
2024-07-19 20:28:52 +0900

    Feat: API Connect-CardList

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -2,19 +2,48 @@ import React, { useState } from 'react';
 import { _Card, Card_Title, Card_Title_Open } from './assets/css/Card.css';
 import TaskList from './TaskList';
 
-function Card({ no, title, description, tasks }) {
-    const [showDetail, setShowDetail] = useState(false);
+function Card({ no, title, description }) {
+    const [showContent, setShowContent] = useState(false);
+    const [tasks, setTasks] = useState([]);
 
     return (
         <div className={_Card}>
             <div
-                className={[Card_Title, `${showDetail && Card_Title_Open}`].join(' ')}
-                onClick={() => {setShowDetail(!showDetail);}}
+                className={[Card_Title, `${showContent && Card_Title_Open}`].join(' ')}
+                // onClick={() => {setShowDetail(!showContent);}}
+                onClick={async (e) => {
+                    if(!showContent) {
+                        try {
+                            const response = await fetch('/api/card', {
+                                method: 'get',
+                                headers: {
+                                    'Accept': 'application/json',
+                                }
+                            });
+
+                            if(!response.ok) {
+                                throw new Error(`${response.status} ${response.statusText}`)
+                            }
+                            
+                            const json = await response.json();
+
+                            if(json.result !== 'success') {
+                                throw new Error(`${json.result} ${json.message}`)
+                            }
+
+                            setTasks(json.data);
+                        } catch(err) {
+                            console.error(err);
+                        }
+                    }
+
+                    setShowContent(!showContent);
+                }}
             >
             {title}
             </div>
             <div>{description}</div>
-            {showDetail && <TaskList tasks={tasks} />}
+            {showContent && <TaskList tasks={tasks} />}
         </div>
     );
 };
--- frontend/src/KanbanBoard.js
+++ frontend/src/KanbanBoard.js
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Kanban_Board } from './assets/css/KanbanBoard.css';
 import CardList from './CardList';
-import cards from './assets/json/data';
+// import cards from './assets/json/data';
 
 function KanbanBoard() {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        fetchCards();
+    }, []);
+
+    const fetchCards = async () => {
+        try {
+            const response = await fetch('/api/card', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: null,
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP Error: ${response.status}`);
+            }
+
+            const json = await response.json();
+
+            if (json.result !== 'success') {
+                throw new Error(`API request Error ${json.message}`);
+            } else {
+                setCards(json.data || []);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    // const fetchTasks = async (cardNo, taskName) => {
+    //     try {
+    //         const response = await fetch('/api/addTask', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Accept': 'application/json',
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({
+    //                 name: taskName,
+    //                 done: 'false',
+    //                 card_no: cardNo,
+    //             }),
+    //         });
+
+    //         if (!response.ok) {
+    //             throw new Error(`HTTP Error: ${response.status}`);
+    //         }
+
+    //         const json = await response.json();
+
+    //         if (json.result !== 'success') {
+    //             throw new Error(`API request Error ${json.message}`);
+    //         } else {
+    //             fetchCards();
+    //         }
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // };
+
     return (
         cards && (
             <div className={Kanban_Board}>
-                <CardList
-                    key={'ToDo'}
-                    title={'ToDo'}
-                    cards={cards.filter((card) => card.status === 'ToDo')}
-                    status={'ToDo'}
-                />
-                <CardList
-                    key={'Doing'}
-                    title={'Doing'}
-                    cards={cards.filter((card) => card.status === 'Doing')}
-                    status={'Doing'}
-                />
-                <CardList
-                    key={'Done'}
-                    title={'Done'}
-                    cards={cards.filter((card) => card.status === 'Done')}
-                    status={'Done'}
-                />
+                <CardList key={'ToDo'} title={'ToDo'} cards={cards.filter((card) => card.status === 'ToDo')} status={'ToDo'} />
+                <CardList key={'Doing'} title={'Doing'} cards={cards.filter((card) => card.status === 'Doing')} status={'Doing'} />
+                <CardList key={'Done'} title={'Done'} cards={cards.filter((card) => card.status === 'Done')} status={'Done'} />
             </div>
         )
     );
